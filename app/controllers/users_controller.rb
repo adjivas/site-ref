85672d20eba38f63425f84aadf377d0a2de26c6a
adjivas <adjivas@users.noreply.github.com>
2016-06-28 21:54:35 +0000

    retry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,7 @@ class UsersController < ApplicationController
         format.html { redirect_to @user, notice: 'User was successfully updated. ' }
         format.json { render :show, status: :ok, location: @user }
       else
+        Rails.logger.debug("Fail Object:")
         format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
