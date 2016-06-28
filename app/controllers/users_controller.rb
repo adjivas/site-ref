3781afb7b3513d5534db01fb98748d8e8a228584
adjivas <adjivas@users.noreply.github.com>
2016-06-28 21:49:49 +0000

    retry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        Rails.logger.debug("My object: #{@user.to_json}")
+        Rails.logger.debug("My object: #{@user.to_json} Second: #{user_params.to_json} ")
         format.html { redirect_to @user, notice: 'User was successfully updated. ' }
         format.json { render :show, status: :ok, location: @user }
       else
